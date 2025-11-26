d52590d50646db892d988b1f9df5db0abc925244
Vitor Hugo <vitorfioriv@gmail.com>
2025-11-26 20:55:57 -0300

    Commit 4

--- Customer.java
+++ Customer.java
@@ -2,44 +2,44 @@ import java.util.Enumeration;
 import java.util.Vector;
 
 public class Customer {
-    private String name;
-    private Vector rentals = new Vector();
+    private String _name;
+    private Vector _rentals = new Vector();
 
     public Customer(String name) {
-        this.name = name;
+        _name = name;
     }
 
     public void addRental(Rental arg) {
-        rentals.addElement(arg);
+        _rentals.addElement(arg);
     }
 
     public String getName() {
-        return name;
+        return _name;
     }
 
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();
+        Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
 
-        while (enum_rentals.hasMoreElements()) {
-            Rental each = (Rental) enum_rentals.nextElement();
-
-            double thisAmount = each.getCharge();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
 
             frequentRenterPoints++;
-
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) 
-                && each.getDaysRented() > 1)
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                 each.getDaysRented() > 1)
                 frequentRenterPoints++;
 
-            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle() + "\t"
+                    + String.valueOf(each.getCharge()) + "\n";
+
+            totalAmount += each.getCharge();
         }
 
-        result += "Amount owed is " + totalAmount + "\n";
-        result += "You earned " + frequentRenterPoints + " frequent renter points";
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints)
+                + " frequent renter points";
 
         return result;
     }
